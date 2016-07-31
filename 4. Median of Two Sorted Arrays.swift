8a6d57d8f628cadbb3f1437e3991844e283e87d4
zhugejunwei <zhugerock@gmail.com>
2016-07-31 18:22:04 -0400

    modify median of two array solution code

--- 4. Median of Two Sorted Arrays.swift
+++ 4. Median of Two Sorted Arrays.swift
@@ -1,43 +1,33 @@
 import Darwin
 
 
-// Bad Solution, 236 ms
-
-func findMedianSortedArrays(nums1: [Int], _ nums2: [Int]) -> Double {
-    var num = nums1 + nums2
-    num = num.sort()
-    if num.count & 0b01 == 1 {
-        return Double(num[(num.count-1)>>1])
-    }else {
-        return Double(num[num.count>>1] + num[num.count>>1-1])/2
-    }
-}
-
-
 // O(log(min(M,N)))
-// 104 ms
+// 92 ms
 
 func findMedianSortedArrays(nums1: [Int], _ nums2: [Int]) -> Double {
-    if nums1.count < nums2.count {
-        return findMedianSortedArrays(nums2, nums1) // nums2 smaller
+    let n1 = nums1.count
+    let n2 = nums2.count
+    if n1 < n2 {
+        return findMedianSortedArrays(nums2, nums1)
     }
-    if nums2.isEmpty {
-        return Double(nums1[(nums1.count-1)>>1] + nums1[(nums1.count)>>1])/2
+    if n2 == 0 {
+        return (Double(nums1[(n1 - 1)/2]) + Double(nums1[n1/2]))/2
     }
-    var lo = 0, hi = nums2.count*2
-    while (lo <= hi) {
-        let mid2 = (lo + hi) / 2
-        let mid1 = nums1.count + nums2.count - mid2
-        let l1:Double = mid1 <= 0 ? Double(INT16_MIN) : Double(nums1[(mid1-1)/2])
-        let l2:Double = mid2 <= 0 ? Double(INT16_MIN) : Double(nums2[(mid2-1)/2])
-        let r1:Double = (mid1 >= nums1.count*2) ? Double(INT32_MAX) : Double(nums1[mid1/2])
-        let r2:Double = (mid2 >= nums2.count*2) ? Double(INT32_MAX) : Double(nums2[mid2/2])
+    var lo = 0, hi = n2*2
+    while lo <= hi {
+        let mid2 = (lo + hi)/2
+        let mid1 = n1 + n2 - mid2
+        let l1 = mid1 == 0 ? Int.min : nums1[(mid1-1)/2]
+        let l2 = mid2 == 0 ? Int.min : nums2[(mid2-1)/2]
+        let r1 = mid1 == 2*n1 ? Int.max : nums1[mid1/2]
+        let r2 = mid2 == 2*n2 ? Int.max : nums2[mid2/2]
+        
         if l1 > r2 {
             lo = mid2 + 1
         }else if l2 > r1 {
             hi = mid2 - 1
         }else {
-            return Double(max(l1,l2) + min(r1,r2))/2
+            return (Double(max(l1,l2)) + Double(min(r1,r2))) / 2
         }
     }
     return -1
